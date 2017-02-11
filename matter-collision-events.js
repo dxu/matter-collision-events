08d3d2a331f0dd872bf38ef68a3cb93d89489084
David <xudavidb@gmail.com>
2017-02-10 21:04:10 -0800

    change collide methods to take callbacks instead of assignment

--- matter-collision-events.js
+++ matter-collision-events.js
@@ -4,22 +4,44 @@
     version: '0.0.0',
     for: 'matter-js@^0.12.0',
     install: function(matter) {
+      // add the onCollide, onCollideEnd, and onCollideActive callback handlers
+      // to the native Matter.Body created
+      var create = matter.Body.create;
+      matter.Body.create = function() {
+        var body = create.apply(null, arguments);
+        body.onCollide = function(cb) { body._mceOC = cb; }
+        body.onCollideEnd = function(cb) { body._mceOCE = cb; }
+        body.onCollideActive = function(cb) { body._mceOCA = cb; }
+        return body;
+      }
       matter.after('Engine.create', function() {
         matter.Events.on(this, 'collisionStart', function(event) {
           event.pairs.map(function(pair) {
             matter.Events.trigger(pair.bodyA, 'onCollide', { pair : pair });
             matter.Events.trigger(pair.bodyB, 'onCollide', { pair : pair });
-            pair.bodyA.onCollide && pair.bodyA.onCollide(pair)
-            pair.bodyB.onCollide && pair.bodyB.onCollide(pair)
+            pair.bodyA._mceOC &&
+              pair.bodyA._mceOC(pair)
+            pair.bodyB._mceOC &&
+              pair.bodyB._mceOC(pair)
           });
         });
 
         matter.Events.on(this, 'collisionActive', function(event) {
           event.pairs.map(function(pair) {
-            matter.Events.trigger(pair.bodyA, 'onCollideActive', { pair : pair });
-            matter.Events.trigger(pair.bodyB, 'onCollideActive', { pair : pair });
-            pair.bodyA.onCollideActive && pair.bodyA.onCollideActive(pair)
-            pair.bodyB.onCollideActive && pair.bodyB.onCollideActive(pair)
+            matter.Events.trigger(
+              pair.bodyA,
+              'onCollideActive',
+              { pair: pair }
+            );
+            matter.Events.trigger(
+              pair.bodyB,
+              'onCollideActive',
+              { pair: pair }
+            );
+            pair.bodyA._mceOCA &&
+              pair.bodyA._mceOCA(pair)
+            pair.bodyB._mceOCA &&
+              pair.bodyB._mceOCA(pair)
           });
         });
 
@@ -27,13 +49,16 @@
           event.pairs.map(function(pair) {
             matter.Events.trigger(pair.bodyA, 'onCollideEnd', { pair : pair });
             matter.Events.trigger(pair.bodyB, 'onCollideEnd', { pair : pair });
-            pair.bodyA.onCollideEnd && pair.bodyA.onCollideEnd(pair)
-            pair.bodyB.onCollideEnd && pair.bodyB.onCollideEnd(pair)
+            pair.bodyA._mceOCE &&
+              pair.bodyA._mceOCE(pair)
+            pair.bodyB._mceOCE &&
+              pair.bodyB._mceOCE(pair)
           });
         });
       });
     },
   };
+
   if (typeof exports !== 'undefined') {
     if (typeof module !== 'undefined' && module.exports) {
       exports = module.exports = MatterCollisionEvents;
